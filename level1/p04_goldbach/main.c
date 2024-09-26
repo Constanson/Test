6eda61b2124155c3fa88d92feeacd95e6509b515
Constanson <17750353666@163.com>
2024-09-26 09:51:59 +0800

    Update main.c

--- level1/p04_goldbach/main.c
+++ level1/p04_goldbach/main.c
@@ -1,6 +1,20 @@
-#include <printf.h>
+#include <stdio.h>
 
 int main() {
-    printf("hello world!\n");
+    printf("4=2+2\n");
+    int arr[24] = {3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
+int tem = 0;
+    for (int i = 4; i < 102; i=i+2) {
+        for (int j = 0; j < 24; j++) {
+            tem=i-arr[j];
+            for (int k = 0; k < 24; k++) {
+                if (arr[k] == tem) {
+                    printf("%d=%d+%d\n",i,arr[j], arr[k]);
+                    tem=0;
+                    goto next1;
+                }
+            }
+        }next1:
+    }
     return 0;
 }
