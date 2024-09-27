7cc1174558f0253a5a902e4ec082b33cc309869e
Constanson <17750353666@163.com>
2024-09-27 23:21:42 +0800

    Update main.c

--- level1/p05_encrypt_decrypt/main.c
+++ level1/p05_encrypt_decrypt/main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+ char str[1000];
+char str1[1000];
+    char str2[1000];
+void encoder() {
+    int len = strlen(str);
+    for (int i = 0; i < len; i++) {
+        str1[i] = ((str[i] - 32 + 60) % 95 + 32);
+    }
+}
+void decoder() {
+    int len = strlen(str);
+    for (int i = 0; i < len; i++) {
+        str2[i] = ((str[i] - 32 +95- 60) % 95 + 32);
+    }
+}
 
 int main() {
-    printf("hello world!\n");
+
+
+
+    printf("Enter a string:\n ");
+    gets(str);
+    encoder();
+    decoder();
+    decoder();
+
+
+
+
+
+    printf("incoded:%s\ndecoded:%s",str1,str2);
     return 0;
 }
