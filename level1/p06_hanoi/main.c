166ed3252eeb2e0050f71751b76f2031e3cf72b7
Constanson <17750353666@163.com>
2024-09-30 13:39:47 +0800

    Update main.c

--- level1/p06_hanoi/main.c
+++ level1/p06_hanoi/main.c
@@ -1,6 +1,36 @@
-#include <printf.h>
+#include <stdio.h>
 
-int main() {
-    printf("hello world!\n");
-    return 0;
+unsigned long long k=0;
+void hanoi(char a, char b ,char c ,int m) ;
+void print (char a , char b );
+int main(){
+        int n;
+    printf("Enter the number of disks: ");
+    scanf("%d", &n);
+      char a='A';
+    char b='B';
+    char c='C';
+
+
+        hanoi(a,b,c,n);
+
+printf("%llutimes in total",k);
+        return 0;
+    }
+
+void hanoi(char a, char b ,char c ,int m) {
+    /*
+ * 将m个盘从a移动到c
+ */if (m == 1) {
+        print (a , c);
+    } else {
+        hanoi(a,c,b,m-1);
+        printf("%c->%c \n",a,c);
+        k++;
+        hanoi(b,a,c,m-1);
+    }
+}
+void print (char a, char b ) {
+    printf("%c->%c \n", a, b);
+    k++;
 }
